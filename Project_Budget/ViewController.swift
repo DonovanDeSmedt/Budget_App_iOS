aca8d1345ee4445d99e90cb59a27d319d96afef0
Donovan De Smedt <donovandesmedt@live.be>
2016-11-11 16:36:29 +0100

    Initial Commit

---
+++ Project_Budget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project_Budget
+//
+//  Created by Donovan De Smedt on 11/11/16.
+//  Copyright © 2016 Donovan De Smedt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
